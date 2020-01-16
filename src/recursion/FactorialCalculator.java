b1d6b86cfb96028588928d2de500f60572a4f5d3
dbc2201 <divyanshb22@gmail.com>
2020-01-16 08:55:15 +0530

    add base condition to the recursive method

--- src/recursion/FactorialCalculator.java
+++ src/recursion/FactorialCalculator.java
@@ -9,6 +9,10 @@ package recursion;
 
 public class FactorialCalculator {
     public int calculateFactorial(int number) {
+        // base condition
+        if (number == 0 || number == 1) {
+
+        }
         return 0;
     }
 }
